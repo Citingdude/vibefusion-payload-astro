9631a0bac67a1682c61ee8992b89660376567c55
glenn <glenn.reumers@hotmail.com>
2026-08-22 19:55:16 +0200

    Refactor code structure for improved readability and maintainability

--- apps/astro/eslint.config.mjs
+++ apps/astro/eslint.config.mjs
@@ -1,5 +1,36 @@
-import antfu from '@antfu/eslint-config'
+import js from '@eslint/js'
+import astro from 'eslint-plugin-astro'
+import format from 'eslint-plugin-format'
+import tseslint from 'typescript-eslint'
 
-export default antfu({
-  astro: true,
-})
+export default [
+  {
+    ignores: ['.astro/**', 'dist/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...astro.configs['flat/recommended'],
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      parser: format.parserPlain,
+    },
+    plugins: {
+      format,
+    },
+    rules: {
+      'format/prettier': ['error', {
+        endOfLine: 'auto',
+        parser: 'astro',
+        plugins: ['prettier-plugin-astro'],
+        printWidth: 120,
+        semi: false,
+        singleAttributePerLine: true,
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        useTabs: false,
+      }],
+    },
+  },
+]
